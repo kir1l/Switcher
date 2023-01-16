5fc5708ea2f2e3e85adac4b3881c900441e3f377
Kirill Balushin <balusinkirill@gmail.com>
2023-01-16 13:10:06 +0300

    Update background.ts

--- background.ts
+++ background.ts
@@ -1,9 +1,5 @@
 var contentTabId: number
 chrome.runtime.onMessage.addListener(function (msg, sender) {
-	console.log(
-		`Received message in background from ${msg.from}, data: `,
-		msg.data
-	)
 	if (msg.from == 'content') {
 		//get content scripts tab id
 		contentTabId = sender.tab!.id!
